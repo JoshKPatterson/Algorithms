3b31d9ec23df01bb15a830b632b7a288f09fa64b
Joshua Patterson <joshkpatt@gmail.com>
2020-12-04 10:33:09 -0500

    Update ReverseBytes.js

--- ReverseBytes.js
+++ ReverseBytes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //This algorithm will reverse the order of an array of bytes while maintaining the bytes
 //
 // 9/14/2020
@@ -12,19 +11,3 @@ function dataReverse(data) {
   
   let returnData = [].concat.apply([], reverse);
   return returnData;
-=======
-//This algorithm will reverse the order of an array of bytes while maintaining the bytes
-//
-// 9/14/2020
-
-function dataReverse(data) {
-  let reverse = [];
-
-  for(var i = 0; data.length > 0 / 8; i++){
-    reverse.unshift(data.splice(0,8))
-  }
-  
-  let returnData = [].concat.apply([], reverse);
-  return returnData;
->>>>>>> 1ebf79311f1f5b274079395590a84ae5f11a7f9b
-}
